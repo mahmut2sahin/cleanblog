d5e1867f3346a35fb4b63054e153b39f9b7f697b
Mahmut Sahin <mahmut2sahin@gmail.com>
2021-05-12 02:15:46 +0300

    bolum-2-tamam

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
+/*
+Clean Blog projemizin bu 2. bölümünde aşağıdaki işlemleri yapalım.
+
+Public klasörü oluşturup statik dosyalarımızı içerisine yerleştirelim.
+İlgili middleware fonksiyonunu yazarak public klasörümüzü uygulamamıza kaydedelim.
+EJS modülünü indirelim.
+Uygulamamızda EJS modülünü kullanacağımızı belirtelim.
+Views klasörü oluşturalım ve tüm .html dosyalarımız views klasörü içerisinde .ejs dosyalarına çevirelim.
+Partials klasör yapısını oluşturalım.
+İlgili yönlendirmeleri ve _navigation.ejs klasöründeki link düzenlemelerini yapalım.
+*/
+
 const express = require("express");
+const ejs = require('ejs');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog Title",
-    description: "Blog Description",
-  };
+// Template Engine
+app.set("view engine", "ejs");
 
-  res.send(blog);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+app.get("/index", (req, res) => {
+  res.render("index")
+});
+
+app.get("/about", (req, res) => {
+  res.render("about")
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post")
+});
+
+app.get("/post", (req, res) => {
+  res.render("post")
 });
 
 const port = 3000;
